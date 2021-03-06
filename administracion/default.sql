7dfcfabd41327a48183e2bae3fb13424c837daaa
Guadalupe Orlando <39423969+Orlandroid@users.noreply.github.com>
2021-03-05 23:09:45 -0600

    Add files via upload

---
+++ administracion/default.sql
@@ -0,0 +1,5 @@
+insert into carrera(carrera) 
+values ('Administracion'),('Industrial'),('Mecatronica'),('Sistemas');
+
+insert into usuarios(usuario,contraseña,tipo)
+values ('usuario','usuario','Administrador');
